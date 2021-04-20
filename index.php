f8b5e0b70458353a5fc596b9976a72a777838ab5
fajarsid <fajarsuganda79@gmail.com>
2021-04-21 05:52:58 +0700

    creat form

--- index.php
+++ index.php
@@ -4,17 +4,30 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
+    <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet" />
+    <script src="https://kit.fontawesome.com/a81368914c.js"></script>
     <!-- css -->
     <link rel="stylesheet" href="css/style.css" />
     <title>Login</title>
   </head>
   <body>
+    <?php
+    if(isset($_GET['pesan'])){
+      if($_GET['pesan'] == "gagal"){
+      echo "Login gagal! username dan password salah!";
+      }else if($_GET['pesan'] == "logout"){
+      echo "Anda telah berhasil logout";
+      }else if($_GET['pesan'] == "belum_login"){
+      echo "Anda harus login untuk mengakses halaman admin";
+      }
+    }
+    ?>
+
     <!-- Form Login -->
     <section>
       <div class="container">
         <div class="login-content">
-          <form action="index.html">
+          <form action="auth.php" method="post" onSubmit="return validasi ()">
             <img src="img/users.png" />
             <h2 class="title">Selamat Datang</h2>
             <div class="input-div one">
@@ -44,5 +57,6 @@
     <!-- Akhir Login -->
 
     <script type="text/javascript" src="js/main.js"></script>
+    <script type="text/javascript" src="js/login.js"></script>
   </body>
 </html>
